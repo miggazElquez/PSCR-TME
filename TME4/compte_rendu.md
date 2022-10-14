efd6cc05223d32596b42d8364a01a0f6249d650f
miggazElquez <miguel.gazquez82@gmail.com>
2022-10-14 11:06:42 +0200

    TME4 Q2

---
+++ TME4/compte_rendu.md
@@ -0,0 +1,11 @@
+TME 4
+=====
+
+Q2)
+---
+
+Le comportement est correct, il n'y a pas de dataraces.
+
+
+Q3)
+---
--- TME4/src/main.cpp
+++ TME4/src/main.cpp
@@ -1,10 +1,39 @@
 #include "Banque.h"
+#include <iostream>
 
 using namespace std;
+using namespace pr;
 
 const int NB_THREAD = 10;
+const int K = 20;
+
+
+
+void work(Banque b, int id) {
+	for (int k=0;k<1000;k++) {
+		size_t j = rand() % K;
+		size_t i = rand() % K;
+		int m = rand() % 99 + 1;
+
+		b.transfert(i,j,m);
+		std::this_thread::sleep_for(std::chrono::milliseconds(rand()%20));
+	}
+
+}
+
+
 int main () {
 	vector<thread> threads;
+
+	::srand(::time(nullptr));
+
+
+	Banque b(K,100);
+
+
+	for (int i=0;i<NB_THREAD;++i) {
+		threads.emplace_back(work,b,i);
+	}
 	// TODO : creer des threads qui font ce qui est demandé
 
 
@@ -12,6 +41,7 @@ int main () {
 		t.join();
 	}
 
-	// TODO : tester solde = NB_THREAD * JP
+	cout << b.comptabiliser(K*100);
+
 	return 0;
 }
