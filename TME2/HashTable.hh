8fc99d76ab1f594d541fafb92f6e51ce06077b9a
miggazElquez <miguel.gazquez82@gmail.com>
2022-10-14 10:41:06 +0200

    TME 3 - iterator. WIP

--- TME2/HashTable.hh
+++ TME2/HashTable.hh
@@ -52,13 +52,91 @@ public:
 
 		size_t h = std::hash<K>()(key);
 		auto& l = buckets[h % size];
-		Entry e(key,value);
+		Entry<K,V> e(key,value);
 		l.push_front(e);
 
 		return false;
 
 	}
 
+
+	class Iterator :public std::iterator<std::input_iterator_tag, Entry<K,V>,long,const Entry<K,V>*, Entry<K,V>> {
+	private:
+		typename std::vector<forward_list<Entry<K,V>>>::iterator cur_b;
+		typename std::vector<forward_list<Entry<K,V>>>::iterator end;
+		typename forward_list<Entry<K,V>>::iterator cur;
+
+	public:
+
+		//struct iterator_category :  std::input_iterator_tag {};
+
+
+		//using iterator_category = std::input_iterator_tag;
+
+		Iterator(typename std::vector<forward_list<Entry<K,V>>>::iterator c_b,
+			typename std::vector<forward_list<Entry<K,V>>>::iterator e, typename forward_list<Entry<K,V>>::iterator c):
+			cur_b(c_b),end(e),cur(c){}
+
+		Iterator operator++() {
+			cur++;
+			if (cur != cur_b->end()) {
+				return *this;
+			}
+			cur_b++;
+			while (cur_b != end) {
+				if (cur_b->begin() != cur_b->end()) {
+					cur = cur_b->begin();
+					break;
+				}
+				cur_b++;
+			}
+			return *this;
+		}
+
+		bool operator!=(Iterator h) {
+			if (cur_b != h.cur_b) {
+				return true;
+			}
+			return (cur != h.cur);
+		}
+
+		bool operator==(Iterator h) {
+			return !(this != h);
+		}
+
+
+		Entry<K,V> operator*() {
+			return *cur;
+		}
+
+	};
+
+	Iterator begin() {
+		auto it = buckets.begin();
+		int i = 0;
+		while (it != buckets.end()) {
+			std::cout << i << std::endl;
+			i++;
+			if (it->begin() != it->end()) {
+				Iterator h(it,buckets.end(),it->begin());
+				return h;
+			}
+		}
+		return end();
+	}
+
+	Iterator end() {
+		Iterator h(buckets.end(),buckets.end(),buckets[size-1].end());
+		return h;
+	}
+
+
+
+
 };
+
+
+
+
 
 }
--- TME2/main.cpp
+++ TME2/main.cpp
@@ -3,6 +3,7 @@
 #include <regex>
 #include <chrono>
 #include <vector>
+#include <algorithm>
 
 #include "HashTable.hh"
 #include "utils.hh"
@@ -23,8 +24,8 @@ int main () {
 	// une regex qui reconnait les caractères anormaux (négation des lettres)
 	regex re( R"([^a-zA-Z])");
 
-	//HashTable<string,int> ht(30000);
-	vector<string> v;
+	HashTable<string,int> ht(30000);
+	//vector<string> v;
 
 	while (input >> word) {
 		// élimine la ponctuation et les caractères spéciaux
@@ -34,17 +35,17 @@ int main () {
 
 		// word est maintenant "tout propre"
 
-		/*int *val = ht.get(word);
+		int *val = ht.get(word);
 
 		if (val) {
 			ht.put(word,*val + 1);
 		} else {
 			ht.put(word,1);
 			nombre_lu++;
-		}*/
+		}
 
 
-		v.push_back(word);
+		//v.push_back(word);
 
 		// if (nombre_lu % 100 == 0)
 		// 	// on affiche un mot "propre" sur 100
@@ -53,6 +54,14 @@ int main () {
 	}
 
 
+	vector<Entry<string,int>> v_final(ht.begin(),ht.end());
+
+	sort(v_final,v_final.begin(), v_final.end(),[](Entry<string,int> e1, Entry<string, int> e2) {e1.value < e2.value;})
+
+	/*for (Entry<string,int> e: ht) {
+		cout << e.key << " : " << e.value << endl;
+	}*/
+
 	cout << "Finished Parsing War and Peace" << endl;
 
 	auto end = steady_clock::now();
@@ -60,21 +69,21 @@ int main () {
               << duration_cast<milliseconds>(end - start).count()
               << "ms.\n";
 
-    //cout << "Found a total of " << nombre_lu << " words." << endl;
-    cout << "Found a total of " << count(v.begin(),v.end()) << " words." << endl;
+    cout << "Found a total of " << nombre_lu << " words." << endl;
+    //cout << "Found a total of " << count(v.begin(),v.end()) << " words." << endl;
 
 
     for (string word: {"war","peace","toto"}) {
 
-    	/*int *val = ht.get(word);
+    	int *val = ht.get(word);
 
 		if (val) {
 			cout << word << " : " << *val << endl;
 		} else {
 			cout << word << " : 0" << endl;			
-		}*/
+		}
 
-    	cout << word << " : " << count_if_equal(v.begin(),v.end(),word);
+    	//cout << word << " : " << count_if_equal(v.begin(),v.end(),word);
 
     }
 
